adfa5d1663e1cb0d8a986d59c79325f4902eb45b
mercspring <68706508+mercspring@users.noreply.github.com>
2021-02-25 12:06:27 -0800

    added a link to instagram

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -13,6 +13,7 @@ export default function index() {
                         <hr />
                         <p>Email: youremail@mail.com</p>
                         <p>Telephone: (111) 111-1111</p>
+                        <p>Instagram: <a href="https://www.instagram.com/jerryspyrography/">https://www.instagram.com/jerryspyrography/</a></p>
                     </Col>
                 </Row>
             </Container>
